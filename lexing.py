b784d16c8b43f5f9930da49aab1d4ac28063083f
Anhiti Sridhar <psyas21@nottingham.ac.uk>
2023-10-30 19:37:01 +0000

    lexer now tokenizes properly

--- lexing.py
+++ lexing.py
@@ -5,5 +5,21 @@ file = open('test.txt')
 charstream = str(file.readlines())
 
 tokens = word_tokenize(charstream)
+words = [word for word in tokens if word not in ['[', ']', "''", '``']]
+out = []
+number = False
+for word in words:
+    for c in range(0, len(word)):
+        if (word[c]) not in ["1", "0", "'"]:
+            print(word[c])
+            # raise Exception ("syntax error: unidentifiable characters")
+    if word == "'":
+        continue
+    elif word[0] == "'":
+        word = word.replace("'", "")
+        dec = int(word, 2)
+         out.append(dec)
+    else:
+        out.append(word)
 
-for word in tokens:
+print(out)
