0811bf8e81dd49040ed502cf811ffdaea0b5cab5
Anhiti Sridhar <psyas21@nottingham.ac.uk>
2023-10-30 18:28:41 +0000

    removed useless function

--- lexing.py
+++ lexing.py
@@ -1,19 +1,5 @@
 from nltk import word_tokenize
 
-class PeekableStream:
-  def __init__(self, iterator):
-    self.iterator = iter(iterator)
-    self._fill()
-  def _fill(self):
-    try:
-      self.next = next(self.iterator)
-    except StopIteration:
-      self.next = None
-  def move_next(self):
-    ret = self.next
-    self._fill()
-    return ret
-
 tokens = []
 file = open('test.txt')
 charstream = str(file.readlines())
@@ -21,4 +7,3 @@ charstream = str(file.readlines())
 tokens = word_tokenize(charstream)
 
 for word in tokens:
-    
