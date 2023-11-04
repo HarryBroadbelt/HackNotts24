32665bd8e1debb41edadc1b3edd24ac8650b4f4f
Harry Broadbelt <psyhb8@nottingham.ac.uk>
2023-11-04 16:53:51 +0000

    commit

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 
 
 import time
-
+import os
 
 from dungeon import *
 from shop import *
 from commands import *
+from parser import *
 
 def main():
 
     initialCashMoney = 0
 
+    cwd = os.getcwd()
+
+    parser = Parser()
+    parser.readFile("test2.txt")
+
     print("The Official [INSERT NAME HERE] Compiler")
     time.sleep(1)
-    print("Segmentation fault")
-    time.sleep(5)
+    print("Segmentation fault (core dumped)")
+    time.sleep(7)
     print("jk bestie")
+
+
 
 
 
